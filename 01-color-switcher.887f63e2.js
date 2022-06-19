6cc70b79ad1701ca2950684b27c17d7924da6d85
MariaBlyzniuk <95179967+MariaBlyzniuk@users.noreply.github.com>
2022-06-19 13:30:59 +0000

    Deploying to gh-pages from @ MariaBlyzniuk/goit-js-hw-09@6cfcd326add9655d18f48a58016e5c6437767eed 🚀

---
+++ 01-color-switcher.887f63e2.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){(l=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3))&&(e.disabled=!0,t.disabled=!1);console.log(`Interval with id ${l} has start!`)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(l),console.log(`Interval with id ${l} has stopped!`)}));let l=null;t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.887f63e2.js.map
